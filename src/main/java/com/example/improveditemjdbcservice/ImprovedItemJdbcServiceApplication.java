bd09cd99f931bf96e23fc0b751fc7ca17aa15bae
Denis Borisov <mai.borisov.denis@gmail.com>
2024-01-04 21:48:06 +0300

    init

---
+++ src/main/java/com/example/improveditemjdbcservice/ImprovedItemJdbcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.improveditemjdbcservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovedItemJdbcServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImprovedItemJdbcServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/improveditemjdbcservice/ImprovedItemJdbcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.improveditemjdbcservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovedItemJdbcServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
